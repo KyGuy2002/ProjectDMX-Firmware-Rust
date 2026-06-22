cb10c707125e998fb96b33fc111557fc4d5e8017
IEatBeans <kylanjaeger@gmail.com>
2026-06-21 20:23:59 -0700

    working neopixels

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,20 @@ use panic_probe as _;
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::pio_programs::ws2812::{PioWs2812, PioWs2812Program, Grb};
+use smart_leds::RGB8;
+use static_cell::StaticCell;
+
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+const NUM_LEDS: usize = 30;
+
 
 
 #[embassy_executor::main]
@@ -67,6 +81,27 @@ async fn main(_spawner: Spawner) {
     display.flush().unwrap();
 
 
+    // WS2812 / NeoPixel setup on GPIO 16
+    let Pio {
+        mut common,
+        sm0,
+        ..
+    } = Pio::new(p.PIO0, Irqs);
+
+    static PROGRAM: StaticCell<PioWs2812Program<PIO0>> = StaticCell::new();
+    let program = PROGRAM.init(PioWs2812Program::new(&mut common));
+
+    let mut ws2812 = PioWs2812::<PIO0, 0, NUM_LEDS, Grb>::new(
+        &mut common,
+        sm0,
+        p.DMA_CH0,
+        p.PIN_9,
+        program,
+    );
+
+    let mut leds = [RGB8::default(); NUM_LEDS];
+
+
 
     let mut count = 0u32;
 
@@ -75,8 +110,20 @@ async fn main(_spawner: Spawner) {
     loop {
         info!("blink {}", count);
 
+        leds.fill(RGB8 { r: 255, g: 0, b: 0 });
+        ws2812.write(&leds).await;
         Timer::after_millis(500).await;
 
+        leds.fill(RGB8 { r: 0, g: 255, b: 0 });
+        ws2812.write(&leds).await;
+        Timer::after_millis(500).await;
+
+        leds.fill(RGB8 { r: 0, g: 0, b: 255 });
+        ws2812.write(&leds).await;
+        Timer::after_millis(500).await;
+
+        leds.fill(RGB8 { r: 0, g: 0, b: 0 });
+        ws2812.write(&leds).await;
         Timer::after_millis(500).await;
 
         count = count.wrapping_add(1);
